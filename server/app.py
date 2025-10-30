d97f00aa283df4986ecdcf350302c566861d010b
caiotcunha <caiotelescunha2001@gmail.com>
2025-10-30 10:26:59 -0300

    tirando comentários e mudando o path

--- server/app.py
+++ server/app.py
@@ -3,28 +3,28 @@ import json
 import pickle
 
 app = Flask(__name__)
-#app.model = pickle.load(open('../mlGenerator/model.pickle', 'rb'))
+app.model = pickle.load(open('./model.pickle', 'rb'))
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-#@app.route("/api/recommend", methods=["POST"])
-#def recommend():
-#    
-#    data = request.get_json()
-#    data = json.loads(data)
-#    songsSet = set(data["songs"])
-#
-#    recommendations = []
-#    for antecedent, consequent, confid in app.model:
-#        if antecedent.issubset(songsSet):
-#            recommendations.extend(consequent)
-#
-#    response = {
-#        'songs': list(set(recommendations)),
-#        #TODO: deve ser dinamico
-#        'version': '1.0.0',
-#        'model_date': '2025-10-28'
-#    }
-#    return jsonify(response)
+@app.route("/api/recommend", methods=["POST"])
+def recommend():
+    
+    data = request.get_json()
+    data = json.loads(data)
+    songsSet = set(data["songs"])
+
+    recommendations = []
+    for antecedent, consequent, confid in app.model:
+        if antecedent.issubset(songsSet):
+            recommendations.extend(consequent)
+
+    response = {
+        'songs': list(set(recommendations)),
+        #TODO: deve ser dinamico
+        'version': '1.0.0',
+        'model_date': '2025-10-28'
+    }
+    return jsonify(response)
